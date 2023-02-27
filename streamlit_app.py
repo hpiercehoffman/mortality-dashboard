bcca5bbf7f2e6a872c1425757b1ad73c1b515fd6
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-02-27 13:36:40 -0500

    try adding map background

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,20 @@ alc_df = alc_df[alc_df['year_id'] == 1990]
 counties = alt.topo_feature(data.us_10m.url, 'counties')
 source = alc_df
 
+width = 400
+height  = 200
+project = 'equirectangular'
+
+map_background = alt.Chart(source
+).mark_geoshape(
+    fill = '#aaa',
+    stroke = 'white'
+).properties(
+    width = 800,
+    height = 500
+).project('equirectangular')
+
+
 us_mort = alt.Chart(counties).mark_geoshape().encode(
     color='mx:Q'
 ).transform_lookup(
@@ -32,4 +46,9 @@ us_mort = alt.Chart(counties).mark_geoshape().encode(
     height=500
 )
 
-st.altair_chart(us_mort, use_container_width=False)
+chart_mort = alt.vconcat(map_background + us_mort
+    ).resolve_scale(
+        color = 'independent'
+    )
+
+st.altair_chart(chart_mort, use_container_width=False)
