35666834b9ac494057a372d33176e9c69e097b0a
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-02-27 13:22:15 -0500

    move first draft code to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,26 @@ def collect_state_data():
     return state_df
 
 state_df = collect_state_data()
+
+st.write("Mortality rates by county")
+
+alc_df = state_df[state_df.cause_name == 'Alcohol use disorders']
+alc_df = alc_df[alc_df.sex == 'Both']
+alc_df = alc_df[alc_df['year_id'] == 1990]
+
+counties = alt.topo_feature(data.us_10m.url, 'counties')
+source = alc_df
+
+us_mort = alt.Chart(counties).mark_geoshape().encode(
+    color='mx:Q'
+).transform_lookup(
+    lookup='id',
+    from_=alt.LookupData(data=source, key='id', fields=['mx'])
+).project(
+    type='albersUsa'
+).properties(
+    width=800,
+    height=500
+)
+
+st.altair_chart(us_mort)
