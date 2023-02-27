29e2b58199b7396d2bca0f2153a58da6db8a9183
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-02-26 21:18:27 -0500

    fix the functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+from process import *
 
-@st.cache
+@st.cache_data
+def collect_state_data():
+    state_df = read_states()
+    return state_df
+
+state_df = collect_state_data()
