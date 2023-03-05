30919fe7f058b6b5dcffa6a3e07e58f24b4bf484
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-03-05 16:12:47 -0500

    add a geoshape trend chart

--- pages/2_⬆️_trends.py
+++ pages/2_⬆️_trends.py
@@ -8,13 +8,63 @@ import process_data
 # Configure how the page appears in browser tab
 st.set_page_config(page_title="Explore Trends", page_icon="⬆️")
 
-# @st.cache_data
+@st.cache_data
 
 def collect_diff_data():
     diff_df = process_data.read_diff_csv()
     return diff_df
 
+def collect_state_data():
+    state_df = process_data.read_states()
+    state_df = state_df.dropna(subset=['FIPS'])
+    state_df["id"] = state_df["FIPS"].astype(int)
+    return state_df
+
 diff_df = collect_diff_data()
+state_df = collect_state_data()
 
-st.dataframe(diff_df)
+st.title("Explore trends in the data")
 
+with st.sidebar:
+    # Selectbox widget for mortality cause
+    display_cause = st.selectbox(
+        label="Select a mortality cause",
+        options=state_df["cause_name"].unique(),
+        index=0
+    )
+    # Radio buttons to select sex
+    display_sex = st.radio(
+        label="Select a sex to display",
+        options=("Male", "Female", "Both"),
+        index=0
+    )
+
+subset_df = diff_df[diff_df.cause_name == display_cause]
+subset_df = subset_df[subset_df.sex == display_sex]
+
+# Map of the U.S. by counties
+counties = alt.topo_feature(data.us_10m.url, 'counties')
+source_diff = diff_df
+
+# Map showing the US colored by percent change in mortality
+mort_diff = alt.Chart(counties).mark_geoshape().encode(
+    color=alt.Color("pc_change_val:Q", title="Percent change in mortality")
+).transform_lookup(
+    lookup='id',
+    from_=alt.LookupData(data=source_diff,
+                         key='id',
+                         fields=['pc_change_val'])
+).project(
+    "albersUsa"
+).properties(
+    title="Percent Change in Mortality",
+    width=800,
+    height=600
+)
+
+chart_trend = alt.hconcat(mort_diff).resolve_scale(
+    color='independent'
+)
+
+st.altair_chart(chart_trend,
+    use_container_width=False)
