4a479589230cbc796b5c5a3185fb4ce5d5a6d471
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-03-05 10:42:19 -0500

    rename more columns

--- pages/3_📈_poverty.py
+++ pages/3_📈_poverty.py
@@ -8,14 +8,19 @@ import process_data
 # Configure how the page appears in browser tab
 st.set_page_config(page_title="2014 Poverty Rates", page_icon="📈")
 
-# @st.cache_data
+@st.cache_data
 
 def collect_poverty_data():
     poverty_df = process_data.read_poverty_csv()
+    poverty_df = poverty_df.rename({'Poverty Percent, All Ages': 'percent',
+                                    'County FIPS Code': 'id'})
     return poverty_df
 
 poverty_df = collect_poverty_data()
 
 st.write("2014 poverty rates")
+
+# Map of the U.S. by counties
+counties = alt.topo_feature(data.us_10m.url, 'counties')
 
 st.dataframe(poverty_df)
