635b6f3ab62648cf24439425e702178388f2100a
Varun Ullanat <87467265+VarunUllanat@users.noreply.github.com>
2023-03-05 16:52:59 -0500

    Update 1_📊_states.py

--- pages/1_📊_states.py
+++ pages/1_📊_states.py
@@ -17,6 +17,8 @@ def collect_state_data():
     state_df["id"] = state_df["FIPS"].astype(int)
     return state_df
 
+
+
 state_df = collect_state_data()
 
 # Additional processing to filter the dataframe for only states, not counties
@@ -27,7 +29,6 @@ state_to_id = {v:i for (v,i) in zip(only_state_df.State, only_state_df.id) }
 
 # Sidebar for data filtering widgets
 with st.sidebar:
-
     # Multi-select widget for mortality causes
     display_causes = st.multiselect(
         label="Select one or more mortality causes",
@@ -53,10 +54,11 @@ with st.sidebar:
     # Selectbox widget for state to show in detail
     display_state = st.selectbox(
         label="Select a state",
-        options=state_df["State"].unique(),
+        options=['USA'] + state_df["State"].unique().tolist().sort(),
         index=0
     )
-    display_state_id = state_to_id[display_state]
+    if display_state is not 'USA':
+        display_state_id = state_to_id[display_state]
 
 # Main chart title
 st.write("Mortality rates by county")
@@ -66,48 +68,66 @@ subset_df = state_df[state_df.cause_name.isin(display_causes)]
 subset_df = subset_df[subset_df.sex == display_sex]
 subset_df = subset_df[subset_df.year_id == display_year]
 
-# Subset the dataframe to entries belonging to the selected state
-subset_df_state = subset_df[subset_df.State == display_state]
 
 # Map of the U.S. by counties
 counties = alt.topo_feature(data.us_10m.url, 'counties')
-source = subset_df
 
-highlight = alt.selection_single(on='mouseover', fields=['id'], empty='none')
+#highlight = alt.selection_single(on='mouseover', fields=['id'], empty='none')
 
 
 # Main map showing the whole U.S. colored by mortality rate
-us_scale = alt.Scale(domain=[source['mx'].min(), source['mx'].max()])
 
-us_mort = alt.Chart(counties).mark_geoshape().transform_lookup(
-    lookup='id',
-    from_=alt.LookupData(data=source, key='id', fields=['mx'])
-).transform_calculate(
-        state_id = "(datum.id / 1000)|0"
-).encode(
-    color=alt.condition((alt.datum.state_id)==display_state_id, 'mx:Q', alt.value("#E48A3F"))
-).project(
-    "albersUsa"
-).properties(
-    width=500,
-    height=300
-)
-
-map_state =alt.Chart(data = counties).mark_geoshape().transform_calculate(
-        state_id = "(datum.id / 1000)|0"
-    ).transform_filter(
-        (alt.datum.state_id)==display_state_id
-    ).encode(
-        color=alt.Color('mx:Q', title="Deaths per 100,000", scale=us_scale)
+if display_state == 'USA':
+    us_mort = alt.Chart(counties).mark_geoshape(
     ).transform_lookup(
-        lookup='id', 
-        from_=alt.LookupData(data=subset_df_state , key='id', fields=['mx'])
-    ).project("albersUsa").properties(
-        width=600,
+        lookup='id',
+        from_=alt.LookupData(data=subset_df, key='id', fields=['mx'])
+    ).encode(
+        color=alt.Color('mx:Q', title="Deaths per 100,000")
+    ).project(
+        "albersUsa"
+    ).properties(
+        width=500,
         height=300
-    ).add_selection(highlight)
+    )
+    chart_mort = us_mort
+    
 
-chart_mort = alt.vconcat(us_mort, map_state).resolve_scale(
+
+# Subset the dataframe to entries belonging to the selected state
+if display_state is not 'USA':
+    subset_df_state = subset_df[subset_df.State == display_state]
+    us_scale = alt.Scale(domain=[subset_df_state['mx'].min(), subset_df_state['mx'].max()])
+    
+    us_mort = alt.Chart(counties).mark_geoshape(
+    ).transform_lookup(
+        lookup='id',
+        from_=alt.LookupData(data=subset_df, key='id', fields=['mx'])
+    ).transform_calculate(
+        state_id = "(datum.id / 1000)|0"
+    ).encode(
+        color=alt.condition((alt.datum.state_id)==display_state_id, 'mx:Q', alt.value("#E48A3F"), title="Deaths per 100,000")
+    ).project(
+        "albersUsa"
+    ).properties(
+        width=500,
+        height=300
+    )
+    
+    state_mort =alt.Chart(counties).mark_geoshape().transform_calculate(
+            state_id = "(datum.id / 1000)|0"
+        ).transform_filter(
+            (alt.datum.state_id)==display_state_id
+        ).encode(
+            color=alt.Color('mx:Q', title="Deaths per 100,000", scale=us_scale, title="Deaths per 100,000")
+        ).transform_lookup(
+            lookup='id', 
+            from_=alt.LookupData(data=subset_df_state , key='id', fields=['mx'])
+        ).project("albersUsa").properties(
+            width=600,
+            height=300
+        ).add_selection(highlight)
+    chart_mort = alt.vconcat(us_mort, map_state).resolve_scale(
         color = 'independent')
 
 st.altair_chart(chart_mort,
