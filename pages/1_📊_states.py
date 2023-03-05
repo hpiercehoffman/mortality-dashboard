e1030eca66d756eec941c40fc9f0deb23b02f0c8
Varun Ullanat <87467265+VarunUllanat@users.noreply.github.com>
2023-03-05 16:58:16 -0500

    Update 1_📊_states.py

--- pages/1_📊_states.py
+++ pages/1_📊_states.py
@@ -106,7 +106,7 @@ if display_state != 'USA':
     ).transform_calculate(
         state_id = "(datum.id / 1000)|0"
     ).encode(
-        color=alt.condition((alt.datum.state_id)==display_state_id, 'mx:Q', alt.value("#E48A3F"), title="Deaths per 100,000")
+        color=alt.condition((alt.datum.state_id)==display_state_id, 'mx:Q', alt.value("#E48A3F"), title="Deaths per 100,000", scale=us_scale)
     ).project(
         "albersUsa"
     ).properties(
