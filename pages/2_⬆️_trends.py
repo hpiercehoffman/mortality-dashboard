e761b93361ebc139034d4033d87f37a854802b30
Varun Ullanat <87467265+VarunUllanat@users.noreply.github.com>
2023-03-06 04:03:08 -0500

    Update 2_⬆️_trends.py

--- pages/2_⬆️_trends.py
+++ pages/2_⬆️_trends.py
@@ -104,8 +104,7 @@ counties = alt.topo_feature(data.us_10m.url, 'counties')
 mort_diff = alt.Chart(counties).mark_geoshape().transform_calculate(
     state_id = "(datum.id / 1000)|0"
 ).encode(
-    color=alt.Color("pc_change_val:Q", title="Percent change in mortality"),
-    stroke=alt.condition(alt.datum.state_id == display_state_id, alt.value("red"), alt.value("white"))
+    color=alt.condition(alt.datum.state_id == display_state_id, alt.value('red'), "mx:Q")
 ).transform_lookup(
     lookup='id',
     from_=alt.LookupData(data=subset_diff,key='id',fields=['pc_change_val'])
