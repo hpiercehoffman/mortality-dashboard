c333e947b32cbf5133238df0c48dc33134c7b9a1
Varun Ullanat <87467265+VarunUllanat@users.noreply.github.com>
2023-03-06 17:26:58 -0500

    Update 3_📈_poverty.py

--- pages/3_📈_poverty.py
+++ pages/3_📈_poverty.py
@@ -61,8 +61,8 @@ state_to_id = {v:i for (v,i) in zip(only_state_df.State, only_state_df.id) }
 display_state_id = state_to_id[display_state]     
 
 state_df.state_id = [int(i / 1000)|0  for i in state_df.id]
-st.write(subset_df)
-subset_df = subset_df[state_df.cause_name == display_cause]
+st.write(state_df)
+subset_df = state_df[state_df.cause_name == display_cause]
 subset_df = subset_df[subset_df.year_id == display_year]
 
 
