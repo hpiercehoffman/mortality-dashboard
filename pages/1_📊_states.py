04d5ec585fcf05dc89ddea531ab87979ae5b05bc
Varun Ullanat <87467265+VarunUllanat@users.noreply.github.com>
2023-03-05 23:49:28 -0500

    Update 1_📊_states.py

--- pages/1_📊_states.py
+++ pages/1_📊_states.py
@@ -97,9 +97,8 @@ def country_map():
     
 if display_state == 'USA':
     fips = altair_component(altair_chart=country_map()).get("id")
-    st.write(fips[0])
     if fips:
-        state_fips = int(fips[0])/1000|0
+        state_fips = int(fips[0]/1000)|0
         state_mort =alt.Chart(counties).mark_geoshape().transform_calculate(
             state_id = "(datum.id / 1000)|0"
         ).transform_filter(
