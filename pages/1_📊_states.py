96a5a36b00167de44f428568487cadb7b4d370fb
Varun Ullanat <87467265+VarunUllanat@users.noreply.github.com>
2023-03-05 23:33:44 -0500

    Update 1_📊_states.py

--- pages/1_📊_states.py
+++ pages/1_📊_states.py
@@ -77,7 +77,7 @@ counties = alt.topo_feature(data.us_10m.url, 'counties')
 # Main map showing the whole U.S. colored by mortality rate
 @st.cache
 def country_map():
-    selection = alt.selection_single(empty="none")
+    selection = alt.selection_single(fields=['id'], empty="none")
     return (alt.Chart(counties).mark_geoshape(
     ).transform_lookup(
         lookup='id',
