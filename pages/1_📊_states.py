4b605da90dadf6772a235e679bb212b7ffa2a98b
Varun Ullanat <87467265+VarunUllanat@users.noreply.github.com>
2023-03-06 01:43:12 -0500

    Update 1_📊_states.py

--- pages/1_📊_states.py
+++ pages/1_📊_states.py
@@ -81,8 +81,6 @@ def country_map():
     ).properties(
         width=650,
         height=300
-    ).configure_legend(
-        orient='bottom'
     )
     
 st.write("Select a county to see its state view")
