ec732c53b74c1f2c0296048b2e6c983f52879298
Varun Ullanat <87467265+VarunUllanat@users.noreply.github.com>
2023-03-06 03:41:17 -0500

    Update 2_⬆️_trends.py

--- pages/2_⬆️_trends.py
+++ pages/2_⬆️_trends.py
@@ -36,62 +36,62 @@ with st.sidebar:
 
     display_sex = "Both"
     
-#     # Selectbox widget for state to show in line plot
-#     display_state = st.selectbox(
-#         label="Select a state",
-#         options=state_df["State"].unique(),
-#         index=0
-#     )
+    # Selectbox widget for state to show in line plot
+    display_state = st.selectbox(
+        label="Select a state",
+        options=state_df["State"].unique(),
+        index=0
+    )
 
 subset_diff = diff_df[diff_df.cause_name == display_cause]
 subset_diff = subset_diff[subset_diff.sex == display_sex]
 
 subset_state = state_df[state_df.cause_name == display_cause]
 # subset_state = subset_state[subset_state.sex == display_sex]
-# subset_state = subset_state[subset_state.State == display_state]
+subset_state = subset_state[subset_state.State == display_state]
 
-county_to_id = {v:i for (v,i) in zip(subset_state.location_name, subset_state.id)}
-id_to_county = {v: k for k, v in county_to_id.items()}
+# county_to_id = {v:i for (v,i) in zip(subset_state.location_name, subset_state.id)}
+# id_to_county = {v: k for k, v in county_to_id.items()}
 
 # Map of the U.S. by counties
 counties = alt.topo_feature(data.us_10m.url, 'counties')
 
-def ret_fips():
-    from streamlit_vega_lite import altair_component
-    def country_map_diff():
-        selection = alt.selection_single(fields=['id'], empty="none")
-        return (alt.Chart(counties).mark_geoshape().transform_lookup(
-            lookup='id',
-            from_=alt.LookupData(data=subset_diff, key='id', fields=['pc_change_val', 'location_name'])
-        ).encode(
-            color='pc_change_val:Q',
-            tooltip=[alt.Tooltip('location_name:N', title='County Name'),
-                     alt.Tooltip('pc_change_val:Q', title='Percentage change', format='.2f')]
-        ).project(
-            "albersUsa"
-        ).add_selection(selection
-        ).properties(
-            width=600,
-            height=300
-        ))
-    return altair_component(altair_chart=country_map_diff()).get("id")
+# def ret_fips():
+#     from streamlit_vega_lite import altair_component
+#     def country_map_diff():
+#         selection = alt.selection_single(fields=['id'], empty="none")
+#         return (alt.Chart(counties).mark_geoshape().transform_lookup(
+#             lookup='id',
+#             from_=alt.LookupData(data=subset_diff, key='id', fields=['pc_change_val', 'location_name'])
+#         ).encode(
+#             color='pc_change_val:Q',
+#             tooltip=[alt.Tooltip('location_name:N', title='County Name'),
+#                      alt.Tooltip('pc_change_val:Q', title='Percentage change', format='.2f')]
+#         ).project(
+#             "albersUsa"
+#         ).add_selection(selection
+#         ).properties(
+#             width=600,
+#             height=300
+#         ))
+#     return altair_component(altair_chart=country_map_diff()).get("id")
 
-st.write("Select a county to see its trends")
+# st.write("Select a county to see its trends")
 
-fips_c = ret_fips()
-print(fips_c)
-if fips_c:
-    county_fips = fips_c[0]
-    st.write(f"Mortality trends across sexes for {id_to_county[county_fips]}")
-    state_trends = alt.Chart(source_states).mark_line(point=True).encode(
-        x='year_id:O',
-        y='mx:Q',
-        color='sex:N'
-    ).properties(
-        width=800,
-        height=600
-    )
-    st.altair_chart(state_trends)
+# fips_c = ret_fips()
+# print(fips_c)
+# if fips_c:
+#     county_fips = fips_c[0]
+#     st.write(f"Mortality trends across sexes for {id_to_county[county_fips]}")
+#     state_trends = alt.Chart(source_states).mark_line(point=True).encode(
+#         x='year_id:O',
+#         y='mx:Q',
+#         color='sex:N'
+#     ).properties(
+#         width=800,
+#         height=600
+#     )
+#     st.altair_chart(state_trends)
 
 
 # # Map showing the US colored by percent change in mortality
