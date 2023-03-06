a156c4ef447c93cac7fc1bd0afe9512a77cf3ac8
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-03-06 00:34:27 -0500

    main page text

--- Home.py
+++ Home.py
@@ -1,28 +1,25 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Overview",
-    page_icon="💡",
+    page_title="Home",
+    page_icon="🏠",
 )
 
-st.write("# Welcome to Streamlit! 👋")
-
-st.sidebar.success("Select a view to explore")
+st.write("# U.S. Mental Health Mortality")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    ## Varun Ullanat and Hannah Pierce-Hoffman ##
+    ### BMI 706 Final Project ###
+
+    ### Dive into the data ###
+    - Explore data categories in the **States** tab
+    - Visualize data trends in the **Trends** tab
+    - Compare poverty and mortality data in the **Poverty** tab
+
+    ### Learn more about this project ###
+    - Visit [our GitHub repo](https://github.com/hpiercehoffman/bmi706-outliers/blob/main/README.md)
+    - Take a look at [our mortality dataset](https://ghdx.healthdata.org/record/ihme-data/united-states-substance-use-disorders-intentional-injuries-mortality-rates-county-1980-2014)
+    - Take a look at [our poverty dataset](https://www.census.gov/data/datasets/2014/demo/saipe/2014-state-and-county.html)
 """
 )
